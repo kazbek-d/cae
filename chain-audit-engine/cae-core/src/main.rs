69f82d52c7bb9815505ee61e264fbb273596d559
Kazbek DZARASOV <kazbek_d@hotmail.com>
2026-03-27 13:16:21 +0100

    Update tracing subscriber initialization for enhanced debug logging

--- chain-audit-engine/cae-core/src/main.rs
+++ chain-audit-engine/cae-core/src/main.rs
@@ -7,7 +7,10 @@ use sqlx::postgres::PgPoolOptions;
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     dotenv().ok();
-    tracing_subscriber::fmt::init();
+    //tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::INFO)
+    .init();
 
     let db_url = std::env::var("DATABASE_URL")?;
     let pool = PgPoolOptions::new().max_connections(10).connect(&db_url).await?;
